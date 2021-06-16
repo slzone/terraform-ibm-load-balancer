4c28ac727ee68bec24268d4e3ef8abc00278d623
shalkygaur <82538063+shalkygaur@users.noreply.github.com>
2021-06-16 14:41:08 -0500

    Update stage3-loadbalancer.tf

--- test/stages/stage3-loadbalancer.tf
+++ test/stages/stage3-loadbalancer.tf
@@ -1,13 +1,12 @@
 module "lbs" {
   source = "./module"
 
-  create_load_balancer = var.create_load_balancer
-  name                 = var.name
-  subnets              = "${module.subnets.subnets.*.id}"
-  resource_group_id    = module.resource_group.id
-  security_groups      = var.security_groups
-  type                 = var.type
-  profile              = var.profile
-  logging              = var.logging
-  tags                 = var.tags
+  provision         = var.create_load_balancer
+  name              = var.name
+  subnets           = "${module.subnets.subnets.*.id}"
+  resource_group_id = module.resource_group.id
+  security_groups   = var.security_groups
+  type              = var.type
+  profile           = var.profile
+  tags              = var.tags
 }
