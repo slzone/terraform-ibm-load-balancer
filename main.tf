00b055bfd38d82bf99e97d47b496b07083d10f77
shalkygaur <82538063+shalkygaur@users.noreply.github.com>
2021-06-16 14:53:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "ibm_is_lb" "loadbalancer" {
-  count           = var.create_load_balancer ? 1 : 0
+  count           = (var.provision ? 1 : 0)
 
   name            = var.name
   subnets         = var.subnets
   type            = var.type != null ? var.type : "public"
   security_groups = (var.security_groups != null ? var.security_groups : [])
   profile         = (var.profile != null && var.logging == null ? var.profile : null)
-  logging         = (var.logging != null && var.profile == null ? var.logging : false)
   resource_group  = var.resource_group_id
   tags            = (var.tags != null ? var.tags : [])
 }
